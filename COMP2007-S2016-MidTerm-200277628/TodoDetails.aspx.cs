15faa8f984a8a9070dee91f1d0a7212e5d359bb9
Wangzhetuteng <ericdicaprio@outlook.com>
2016-06-23 11:01:53 -0400

    added TodosList page

--- COMP2007-S2016-MidTerm-200277628/TodoDetails.aspx.cs
+++ COMP2007-S2016-MidTerm-200277628/TodoDetails.aspx.cs
@@ -5,13 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// using statements required for EF DB access
+using COMP2007_S2016_MidTerm_200277628.Models;
+using System.Web.ModelBinding;
+
 namespace COMP2007_S2016_MidTerm_200277628
 {
-    public partial class TodoDetails : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-    }
+   
 }
--- COMP2007-S2016-MidTerm-200277628/TodoList.aspx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Page Title="Todo List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TodoList.aspx.cs" Inherits="COMP2007_S2016_MidTerm_200277628.TodoList" %>
-
-<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-     
-</asp:Content>
